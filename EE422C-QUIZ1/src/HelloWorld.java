fbd68347960cbb07fe406de2e85afae0eb3e9578
mina abbassian <mina.abbassian@gmail.com>
2020-08-27 13:50:52 -0500

    initial commit

---
+++ EE422C-QUIZ1/src/HelloWorld.java
@@ -0,0 +1,17 @@
+//HelloWorld.java
+/*
+ * 
+ * EE422C Quiz 1 submission by
+ * Mina Abbassian
+ * mea2947
+ * 16170
+ * Fall 2020
+ */
+
+public class HelloWorld {
+	
+	public static void hello() {
+		System.out.println("Hello World");
+	}
+
+}
---
+++ EE422C-QUIZ1/src/Main.java
@@ -0,0 +1,17 @@
+//Main.java
+/*
+ * 
+ * EE422C Quiz 1 submission by
+ * Mina Abbassian
+ * mea2947
+ * 16170
+ * Fall 2020
+ */
+
+public class Main {
+	
+	public static void main(String[] args) {
+		HelloWorld.hello();
+	}
+
+}
